e6c2114690714b6150fd4f4d6775cc9ba552d72c
Daniyaal Beg <danyaalbeg@gmail.com>
2023-07-31 03:04:22 +0500

    Added OG images

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'D Beg'
-export const SITE_DESCRIPTION = 'A site for all of my projects and ideas.'
+export const SITE_TITLE = 'Daniyaal Beg'
+export const SITE_DESCRIPTION =
+	'My digital home on the internet. Find my projects and tutorials here.'
