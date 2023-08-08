af84fafa89f62da43cef63090410d559cf10df75
Daniyaal Beg <danyaalbeg@gmail.com>
2023-08-08 20:50:35 +0500

    feat: Added posthog analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-import react from '@astrojs/react'
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://daniyaalbeg.com',
-	integrations: [mdx(), sitemap(), tailwind(), react()],
-	experimental: {
-		viewTransitions: true,
-	},
-})
+  site: 'https://daniyaalbeg.com',
+  integrations: [mdx(), sitemap(), tailwind(), react(), partytown()],
+  experimental: {
+    viewTransitions: true
+  }
+});
